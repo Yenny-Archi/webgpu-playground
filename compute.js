dfdaea64d661aa3e2dc313fa175796bbf1acf7fd
Yenny <yenny@archisketch.com>
2025-05-14 16:33:20 +0900

    game of life 알고리즘 추가

--- compute.js
+++ compute.js
@@ -87,9 +87,10 @@ const cellStateStorage = [
   })
 ];
 
-// Mark every third cell of the first grid as active.
-for (let i = 0; i < cellStateArray.length; i+=3) {
-  cellStateArray[i] = 1;
+// Set each cell to a random state, then copy the JavaScript array 
+// into the storage buffer.
+for (let i = 0; i < cellStateArray.length; ++i) {
+    cellStateArray[i] = Math.random() > 0.6 ? 1 : 0;
 }
 device.queue.writeBuffer(cellStateStorage[0], 0, cellStateArray);
 
@@ -151,17 +152,41 @@ const simulationShaderModule = device.createShaderModule({
       @group(0) @binding(2) var<storage, read_write> cellStateOut: array<u32>;
 
       fn cellIndex(cell: vec2u) -> u32 {
-        return cell.y * u32(grid.x) + cell.x;
+        return (cell.y % u32(grid.y)) * u32(grid.x) +
+               (cell.x % u32(grid.x));
+      }
+
+      fn cellActive(x: u32, y: u32) -> u32 {
+        return cellStateIn[cellIndex(vec2(x, y))];
       }
 
       @compute
       @workgroup_size(${WORKGROUP_SIZE}, ${WORKGROUP_SIZE})
       fn computeMain(@builtin(global_invocation_id) cell: vec3u) {
-        if (cellStateIn[cellIndex(cell.xy)] == 1) {
-            cellStateOut[cellIndex(cell.xy)] = 0;
-          } else {
-            cellStateOut[cellIndex(cell.xy)] = 1;
-          }
+         // Determine how many active neighbors this cell has.
+        let activeNeighbors = cellActive(cell.x+1, cell.y+1) +
+            cellActive(cell.x+1, cell.y) +
+            cellActive(cell.x+1, cell.y-1) +
+            cellActive(cell.x, cell.y-1) +
+            cellActive(cell.x-1, cell.y-1) +
+            cellActive(cell.x-1, cell.y) +
+            cellActive(cell.x-1, cell.y+1) +
+            cellActive(cell.x, cell.y+1);
+
+            let i = cellIndex(cell.xy);
+
+            // Conway's game of life rules:
+            switch activeNeighbors {
+              case 2: {
+                cellStateOut[i] = cellStateIn[i];
+              }
+              case 3: {
+                cellStateOut[i] = 1;
+              }
+              default: {
+                cellStateOut[i] = 0;
+              }
+            }
       }`
   });
 
